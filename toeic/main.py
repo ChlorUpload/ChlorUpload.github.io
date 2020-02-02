80b8a2d8e8a50546fefb9915d08a668ed4509bdd
ChlorUpload <chlor.upload@gmail.com>
2020-02-03 05:51:16 +0900

    Add Toeic

---
+++ toeic/main.py
@@ -0,0 +1,17 @@
+findstr = '문항'
+
+document = 0
+
+file = open('.\\data.part5', 'rt', encoding='UTF8')
+lines = file.readlines()
+
+storefile = open('.\\dummy.part5', 'wt', encoding='UTF8')
+
+for line in lines:
+    if findstr in line[:3]:
+        storefile.close()
+        document += 1
+        storefile = open('.\\data\\'+str(document)+'.part5', 'wt', encoding='UTF8')
+    
+    storefile.write(line)
+    
