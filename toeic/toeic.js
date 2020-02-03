120775c4f21e1046d83d6e00a56cf8a02e29978f
ChlorUpload <chlor.upload@gmail.com>
2020-02-03 17:05:13 +0900

    Change confirm button to submit and next button

--- toeic/toeic.js
+++ toeic/toeic.js
@@ -1,12 +1,31 @@
+let windowtitle = document.getElementById("windowtitle");
+let windowtext = document.getElementById("windowtext");
+let btnimgA = document.getElementById("btnimgA");
+let btnimgB = document.getElementById("btnimgB");
+let btnimgC = document.getElementById("btnimgC");
+let btnimgD = document.getElementById("btnimgD");
+let btnimgSubmit = document.getElementById("btnimgSubmit");
+let btnA = document.getElementById("btnA");
+let btnB = document.getElementById("btnB");
+let btnC = document.getElementById("btnC");
+let btnD = document.getElementById("btnD");
+
+let pText = 'quiz_text';
+let aText = 'answer_text';
+
 let selected = 1;
 updateButtons(1);
 
+let quiz_num = 0;
+getNewRandomQuiz();
+
+const showtype_quiz = 1;
+const showtype_answer = 2;
+let showtype = showtype_quiz;
+show();
+
 function updateButtons(num) {
     selected = num;
-    btnimgA = document.getElementById("btnimgA");
-    btnimgB = document.getElementById("btnimgB");
-    btnimgC = document.getElementById("btnimgC");
-    btnimgD = document.getElementById("btnimgD");
     btnimgA.src="res/button_A_disabled.png";
     btnimgB.src="res/button_B_disabled.png";
     btnimgC.src="res/button_C_disabled.png";
@@ -45,10 +64,47 @@ function buttonD() {
     updateButtons(4);
 }
 
-function buttonE() {
-    
+function buttonSubmit() {
+    if (showtype == showtype_quiz)
+    {
+        showtype = showtype_answer;
+    }
+    else if (showtype == showtype_answer)
+    {
+        getNewRandomQuiz();
+        selected = 1;
+        updateButtons(1);
+        showtype = showtype_quiz;
+    }
+    show();
 }
 
-function buttonConfirm() {
+function show() {
 
+    if (showtype == showtype_quiz)
+    {
+        windowtitle.innerHTML = 'Quiz.';
+        windowtext.innerHTML = pText;
+        btnA.style.visibility='visible';
+        btnB.style.visibility='visible';
+        btnC.style.visibility='visible';
+        btnD.style.visibility='visible';
+        btnimgSubmit.src="res/button_submit.png";
+    }
+    else if (showtype == showtype_answer)
+    {
+        windowtitle.innerHTML = 'Answer.';
+        windowtext.innerHTML = aText;
+        btnA.style.visibility='hidden';
+        btnB.style.visibility='hidden';
+        btnC.style.visibility='hidden';
+        btnD.style.visibility='hidden';
+        btnimgSubmit.src="res/button_next.png";
+    }
+}
+
+function getNewRandomQuiz(){
+    quiz_num++;
+    pText = 'quiz'+String(quiz_num);
+    aText = 'answer'+String(quiz_num);
 }
